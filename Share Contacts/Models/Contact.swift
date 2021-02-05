c266803470d20c3e5dfdc7144cbba56f99610eb5
codenamerhu <rndhlovu@meetdchef.com>
2021-02-05 21:44:32 +0200

    contact model

---
+++ Share Contacts/Models/Contact.swift
@@ -0,0 +1,16 @@
+//
+//  Contact.swift
+//  Share Contacts
+//
+//  Created by Rhulani Ndhlovu on 2021/02/05.
+//
+
+import Foundation
+import UIKit
+
+struct Contact {
+    var firstName       : String
+    var lastName        : String
+    var contactImage    : UIImage
+    var cellPhone       : String
+}
