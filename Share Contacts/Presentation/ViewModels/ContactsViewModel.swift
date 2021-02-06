fbda33c0fb60e3145a3e1ef957a8c66cc7594833
codenamerhu <rndhlovu@meetdchef.com>
2021-02-06 21:54:01 +0200

    model and viewmo & viewModel

---
+++ Share Contacts/Presentation/ViewModels/ContactsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ContactsViewModel.swift
+//  Share Contacts
+//
+//  Created by Rhulani Ndhlovu on 2021/02/06.
+//
+
+import Foundation
+
+class ContactsViewModel {
+    
+    func ferchedContactcs() {
+        
+    }
+}
